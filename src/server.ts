9eaf4a16c3455827e4c74274215875a11d7c4533
Anurag Sharma <mrshanksster@gmail.com>
2019-04-21 09:10:20 -0500

    chore: add tslint
    added tslint config

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,26 @@
+import handlebars from 'handlebars';
 import * as hapi from 'hapi';
-
+import vision from 'vision';
 const hapiOptions: hapi.ServerOptions = {
+    debug: { request: ['error'] },
     port: 8008
 };
 
 const server = new hapi.Server(hapiOptions);
 
 const init = async () => {
+    await server.register(vision);
+
+    server.views({
+        engines: {
+            html: handlebars
+        },
+        path: 'templates',
+        relativeTo: __dirname
+    });
     await server.start();
 };
 
 init().then(() => {
-    console.log('Server started');
+    console.log('Server started'); // tslint-disable
 });
